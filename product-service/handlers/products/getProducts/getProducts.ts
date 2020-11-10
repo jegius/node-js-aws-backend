3b7aece9b658404d446d0f6338fe353ea450f729
Jegius <jegius@gmail.com>
2020-11-10 15:02:21 +0400

    removed extra error

--- product-service/handlers/products/getProducts/getProducts.ts
+++ product-service/handlers/products/getProducts/getProducts.ts
@@ -2,14 +2,11 @@ import 'source-map-support/register';
 import {buildResponse, ErrorCode} from "../../common/helpers";
 import {APIGatewayProxyHandler} from "aws-lambda";
 import {productDao} from "../../../dao/product/productDao";
-import {ProductNotFound} from "../../../errors/errors";
 
 export const getAvailableProductsHandler: APIGatewayProxyHandler = async () => {
     try {
         return buildResponse(await productDao.getAll())
     } catch (error) {
-        return error instanceof ProductNotFound
-            ? buildResponse(error.message, ErrorCode.BAD_REQUEST)
-            : buildResponse(error.message, ErrorCode.INTERNAL_SERVER_ERROR)
+        buildResponse(error.message, ErrorCode.INTERNAL_SERVER_ERROR)
     }
 }
