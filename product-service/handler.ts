ef5657b691b8147b1b980bca31733fb16adbb06b
jegius <Flag5ShipGro0P12>
2020-12-13 01:03:48 +0300

    task 9 implemented

--- product-service/handler.ts
+++ product-service/handler.ts
@@ -5,6 +5,7 @@ import {getAvailableProductByIdHandler} from "./handlers/products/getProductById
 import {addProductHandler} from "./handlers/products/addProduct/addProduct";
 import {catalogBatchProcessHandler} from "./handlers/catalogBatchProcess/catalogBatchProcess";
 
+
 export const getAvailableProducts: APIGatewayProxyHandler = getAvailableProductsHandler;
 export const getAvailableProductById: APIGatewayProxyHandler = getAvailableProductByIdHandler;
 export const addProduct: APIGatewayProxyHandler = addProductHandler;
